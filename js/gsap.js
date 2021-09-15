0511054701b31bf4262f27b83b200d07d16eee32
magusin <fanatsy4@gmail.com>
2021-09-15 03:37:43 +0200

    présentation section complete

--- js/gsap.js
+++ js/gsap.js
@@ -1,10 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// const presentation = document.querySelector(".presentation p");
-// const projet = document.querySelector(".projet");
-// const contact = document.querySelector(".contact");
-// const title = document.querySelector(".presentation-title");
-
 gsap.to('.projet', {opacity: 1, duration: 1, backgroundColor: "rgba(120, 120, 120, 0.10)" ,y: 0, x:50, stagger:1.5});
 gsap.to('.presentation-title', {duration:3, scale: 2.2, scrollTrigger: ".presentation-title"});
 gsap.to('.contact-title', {duration:3, scale: 2.2, scrollTrigger: ".contact-title"});
@@ -30,7 +25,23 @@ gsap.to(".contact", {
     backgroundColor: "rgba(120, 120, 120, 0.10)",
     x: 1850,
     scrollTrigger: ".contact",
-})
+});
+gsap.to(".contact-p", {
+    duration: 4,
+    opacity: 1,
+    scrollTrigger: ".contact"
+});
+gsap.to(".projet-p", {
+    duration: 4,
+    opacity: 1,
+    scrollTrigger: ".projet"
+});
+gsap.to(".presentation-p", {
+    duration: 4,
+    opacity: 1,
+    scrollTrigger: ".presentation"
+});
+
 
 
 
