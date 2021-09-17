083e84b77c5efbe45fdf9b786188622b129de694
magusin <fanatsy4@gmail.com>
2021-09-17 20:42:35 +0200

    complete projet section + fix gsap log <=> accueil

--- js/gsap.js
+++ js/gsap.js
@@ -1,46 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
 let timeline = gsap.timeline({})
-// gsap.to('.presentation-title', {duration:3, scale: 2.2, scrollTrigger: ".presentation-title"});
-// gsap.to('.contact-title', {duration:3, scale: 2.2, scrollTrigger: ".contact-title"});
-// gsap.to(".projet-title", {
-//     duration: 3,
-//     scale: 2.2,
-//     scrollTrigger: ".projet-title",
-// });
-// gsap.to(".presentation", {
-//     duration: 1,
-//     backgroundColor: "rgba(120, 120, 120, 0.10)",
-//     x: 2000,
-//     stagger: 1.5,
-// });
-// gsap.to(".projet", {
-//     duration: 1,
-//     backgroundColor: "rgba(120, 120, 120, 0.10)",
-//     x: 2000,
-//     scrollTrigger: ".projet",
-// });
-// gsap.to(".contact", {
-//     duration: 1,
-//     backgroundColor: "rgba(120, 120, 120, 0.10)",
-//     x: 2000,
-//     scrollTrigger: ".contact",
-// });
-// gsap.to(".contact-p", {
-//     duration: 4,
-//     opacity: 1,
-//     scrollTrigger: ".contact"
-// });
-// gsap.to(".projet-p", {
-//     duration: 4,
-//     opacity: 1,
-//     scrollTrigger: ".projet"
-// });
-// gsap.to(".presentation-p", {
-//     duration: 4,
-//     opacity: 1,
-//     scrollTrigger: ".presentation"
-// });
 
 timeline
 .to(".presentation", {
@@ -55,20 +15,28 @@ timeline
 .to('.presentation-title', {duration:1, scale: 2
 },"<");
 
-gsap.to(".projet", {
+gsap.to(".projets", {
     duration: 1,
     backgroundColor: "rgba(120, 120, 120, 0.10)",
     x: 2000,
-    scrollTrigger: ".projet",
+    scrollTrigger: ".projets",
 })
-gsap.to(".projet-p", {
+gsap.to(".projets-p", {
     duration: 4,
     opacity: 1,
-    scrollTrigger: ".projet-p",
-},">1")
-gsap.to('.projet-title', {duration:2, scale: 2, scrollTrigger:".projet-title"
+    scrollTrigger: ".projets",
+},">")
+gsap.to('.projets-title', {duration:2, scale: 2, scrollTrigger:".projets-title"
 },"<");
-
+gsap.to('.projets_container_element-title',{
+    duration: 2,
+    scale: 1.5,
+    scrollTrigger: ".projets"
+})
+gsap.to(".header_nav-right", {
+    duration: 1,
+    opacity: 1,
+})
 gsap.to(".contact", {
     duration: 1,
     backgroundColor: "rgba(120, 120, 120, 0.10)",
@@ -87,25 +55,29 @@ gsap.to(".header_nav_ul", {
     opacity: 1,
 })
 
+
 function triggerToLog() {
     timeline.to('.main', {
         duration: 2,
         opacity: 1,
         x: -2000,
+        y: 0,
     })
     timeline.to(".header_nav", {
         duration: 0.5,
         opacity: 0,
-    })
+    },"<")
+    timeline.to(".header_nav-right", {
+        duration: 0.5,
+        opacity: 0,
+    },">")
     timeline.to(".log", {
         duration: 1,
         backgroundColor: "rgba(120, 120, 120, 0.10)",
-        scrollTrigger: ".log",
     })
     timeline.to('.log-title', {
         duration: 1,
         scale: 2,
-
     },">")
     timeline.to('.log-p', {
         duration: 3,
@@ -122,30 +94,82 @@ function triggerToLog() {
 };
 
 function triggerToAccueil() {
-    timeline.to(".header_nav-left", {
-        duration: 1,
-        opacity: 0,
-    })
     timeline.to('.main', {
-        duration: 1,
+        duration: 2,
         x: 0,
-    },"<")
+        y: 0,
+    })
+    timeline.to(".header_nav", {
+        duration: 1,
+        opacity: 1,
+    },">")
     timeline.to('.header_nav-right', {
         duration: 1,
         opacity: 1,
-    },">")
-    document.querySelector('.header_nav').classList.remove('hidden');
-    timeline.to(".header_nav", {
-        duration: 2,
-        opacity: 1,
     })
     timeline.to('.header_nav-left', {
-        duration: 1,
+        duration: 0.5,
         opacity: 0,
+    },"<")
+    timeline.to('.log-p', {
+        duration: 0.1,
+        opacity: 0,
+    }, "<")
+    timeline.to('.log-title', {
+        duration: 0.1,
+        scale: 1,
     },">")
     document.querySelector('.header_nav-left').classList.add('hidden');
     document.querySelector('.header_nav-right').classList.remove('hidden');
+    document.querySelector('.header_nav').classList.remove('hidden');
+}
 
+function projetNala() {
+    if (document.querySelector('.nala').classList.contains('hidden')) {
+        document.querySelector('.nala').classList.remove('hidden');
+        timeline.to('.nala', {
+            duration: 1.5,
+            opacity: 1,
+        })
+    } else {
+        document.querySelector('.nala').classList.add('hidden');
+        timeline.to('.nala', {
+            duration: 0.1,
+            opacity: 0,
+        })
+    }
+}
+
+function projetCv() {
+    if (document.querySelector('.cv').classList.contains('hidden')) {
+        document.querySelector('.cv').classList.remove('hidden');
+        timeline.to('.cv', {
+            duration: 1.5,
+            opacity: 1,
+        })
+    } else {
+        document.querySelector('.cv').classList.add('hidden');
+        timeline.to('.cv', {
+            duration: 0.1,
+            opacity: 0,
+        })
+    }
+}
+
+function projetEnigma() {
+    if (document.querySelector('.enigma').classList.contains('hidden')) {
+        document.querySelector('.enigma').classList.remove('hidden');
+        timeline.to('.enigma', {
+            duration: 1.5,
+            opacity: 1,
+        })
+    } else {
+        document.querySelector('.enigma').classList.add('hidden');
+        timeline.to('.enigma', {
+            duration: 0.1,
+            opacity: 0,
+        })
+    }
 }
 
 
